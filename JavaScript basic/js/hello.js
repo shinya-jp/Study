46dae95eb6cb907c05aeb5dc5bb39c1206e57f04
shinya-jp <shinya0733@gmail.com>
2020-08-14 16:45:13 +0900

    js基本学習/コメント

--- JavaScript basic/js/hello.js
+++ JavaScript basic/js/hello.js
@@ -1,1 +1,6 @@
-console.log('Hello World! from hello.js')
+// 一行
+// console.log('Hello World! from hello.js')
+
+// 複数行
+/* console.log('Hello World! from hello.js')
+console.log('Hello World! from hello.js') */
