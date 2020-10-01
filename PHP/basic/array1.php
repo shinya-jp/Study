b7b09ed26cd4a3bddcf505f1c32e2518a43630e5
shinya-jp <shinya0733@gmail.com>
2020-10-01 16:58:33 +0900

    配列

---
+++ PHP/basic/array1.php
@@ -0,0 +1,10 @@
+<?php
+$names = ["Andy", "Betty", "Carol"];
+
+// 要素番号は１からではなく、０から始まる
+echo "Hello";
+echo $names[0]; #Andy
+echo "Hello";
+echo $names[1]; #Betty
+echo "Hello";
+echo $names[2]; #Carol
