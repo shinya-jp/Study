7bb6d139440af4990f2ceb6c68a1e603f191cb48
shinya-jp <shinya0733@gmail.com>
2020-10-02 18:35:45 +0900

    要素番号の指定

---
+++ PHP/basic/array2.php
@@ -0,0 +1,11 @@
+<?php
+$names = ["Andy", "Betty", "Carol"];
+
+echo $names[3];
+
+/*
+存在しない要素番号のためエラー表示なる
+
+PHP Notice:  Undefined offset: 3 in /Users/kawaishinnari/Desktop/Study/PHP/basic/array2.php on line 4
+
+Notice: Undefined offset: 3 in /Users/kawaishinnari/Desktop/Study/PHP/basic/array2.php on line 4 */
