b042ef3b4e1aebbccd6441cf9da8e94a315ff0a9
shinya-jp <shinya0733@gmail.com>
2020-10-04 16:15:56 +0900

    反復条件

--- PHP/basic/loop1.php
+++ PHP/basic/loop1.php
@@ -1,7 +1,25 @@
 <?php
 $i = 0;
 // 繰り返し処理
-while ($i < 3) {
+// while ($i = 3) {
+// 	echo $i . PHP_EOL;
+// 	$i++;
+// }
+
+// while ($i <= 0) {
+// 	echo $i . PHP_EOL;
+// 	$i++;
+// }
+// 0のみ出力される
+
+// while ($i > 5) {
+// 	echo $i . PHP_EOL;
+// 	$i++;
+// }
+// iは５より小さいのでなにも実行されない
+
+while (true) {
 	echo $i . PHP_EOL;
 	$i++;
 }
+// 常にtrueなので無限に実行されてしまう
