f325bc01e8bf7a5ab5f2855f58d2f07a0703674c
shinya-jp <shinya0733@gmail.com>
2020-10-04 16:52:58 +0900

    配列と反復構造

--- PHP/basic/loop2.php
+++ PHP/basic/loop2.php
@@ -2,7 +2,9 @@
 $names = ["Andy", "Betty", "Carol"];
 
 $i = 0;
-while ($i < 3) {
+// while ($i < 3) {
+// count変数を使用する方法もある
+while ($i < count($names)) {
 	echo "Hello ";
 	echo $names[$i] . PHP_EOL;
 	$i++;
