e9bd11f71a3f8f7abd2a398de1b63ba760a778c6
shinya-jp <shinya0733@gmail.com>
2020-10-10 10:16:43 +0900

    演習2

---
+++ PHP/basic/choice_tr2.php
@@ -0,0 +1,14 @@
+<?php
+$scores = [90, 72, 58, 80];
+
+for ($i = 0; $i < count($scores); $i++) {
+	$score = $scores[$i];
+
+	if ($score >= 80) {
+		echo "$score : A" . PHP_EOL;
+	}elseif ($score >= 60) {
+		echo "$score : B" . PHP_EOL;
+	}else {
+		echo "$score : C" .PHP_EOL;
+	}
+}
