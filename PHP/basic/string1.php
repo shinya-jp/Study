9cb975835c59249a40d62e4c2361081ca0d47713
shinya-jp <shinya0733@gmail.com>
2020-10-12 22:36:39 +0900

    strlower

--- PHP/basic/string1.php
+++ PHP/basic/string1.php
@@ -1,5 +1,6 @@
 <?php
 // strlen 文字列の長さを得る
+// 日本語の場合は使えない　日本語専用の関数がある
 
 $str = "Hello World";
 $length = strlen($str);
---
+++ PHP/basic/string2.php
@@ -0,0 +1,7 @@
+<?php
+// strtolower 文字列を小文字にする
+
+$str = "Hello World";
+$lower_case = strtolower($str);
+
+echo $lower_case . PHP_EOL;
