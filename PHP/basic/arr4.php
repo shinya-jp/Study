2ce4d291a6c6528bf8524596ad93dd284f9e5bca
shinya-jp <shinya0733@gmail.com>
2020-10-14 21:51:54 +0900

    array_slice

--- PHP/basic/arr4.php
+++ PHP/basic/arr4.php
@@ -1,8 +1,8 @@
 <?php
 $array = ["Andy", "Bety", "Carol"];
 
-// array_alice 配列の一部を展開する
-
-$sliced_array = array_slice($array, 1);
+// array_slice 配列の一部を展開する
+// array_slice(変数, 開始位置, 必要なら終わり位置);
+$sliced_array = array_slice($array, 1, 1);
 
 print_r($sliced_array);
