e33f6e9d331f7e67a244ef3f397c4afeddf6bd17
shinya-jp <shinya0733@gmail.com>
2020-10-14 22:16:55 +0900

    演習

---
+++ PHP/basic/arr_tr1.php
@@ -0,0 +1,7 @@
+<?php
+
+$socres = [90, 72, 58, 80];
+
+$sum = array_sum($socres);
+
+echo $sum;
---
+++ PHP/basic/arr_tr3.php
@@ -0,0 +1,12 @@
+<?php
+
+$names = ["Carol", "Andy", "Betty", "Daniel"];
+
+// sort($names);
+// $reverse_names = array_reverse($names);
+// var_dump($reverse_names);
+
+// 上記のコード一分に
+// rsort関数 ソートを逆の順番に並びかえる
+rsort($names);
+var_dump($names);
