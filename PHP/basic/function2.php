434a1bcd43186eb25b6e0c08471562c34f4ef5fc
shinya-jp <shinya0733@gmail.com>
2020-10-18 22:10:09 +0900

    ユーザー定義関数

---
+++ PHP/basic/function2.php
@@ -0,0 +1,12 @@
+<?php
+// ユーザー定義( オリジナル)関数
+function calc($price) {
+	$result = $price * 1.08;
+	return $result;
+}
+
+$price1 = calc(100);
+echo $price1 . PHP_EOL;
+
+$price2 = calc(1000);
+echo $price2 . PHP_EOL;
