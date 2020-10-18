bfd1bcd2fa2c2c8b68e601158bdb289308d96824
shinya-jp <shinya0733@gmail.com>
2020-10-18 22:17:18 +0900

    引数の定義

---
+++ PHP/basic/function3.php
@@ -0,0 +1,8 @@
+<?php
+// 引数の定義
+function sum($x, $y, $z) {
+	return $x + $y + $z;
+}
+
+$result = sum(100, 200, 300,);
+echo $result . PHP_EOL;
---
+++ PHP/basic/function4.php
@@ -0,0 +1,8 @@
+<?php
+// 引数のないユーザー定義関数
+function hello() {
+	return "Hello";
+}
+
+$result = hello();
+echo $result;
