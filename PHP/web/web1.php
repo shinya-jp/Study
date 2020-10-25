622f50ca9d2d1c5687086eb569b839c5b7c8b3b7
shinya-jp <shinya0733@gmail.com>
2020-10-25 17:39:12 +0900

    webアプリケーション

---
+++ PHP/web/web1.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Andy";
+?>
+
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+	<meta charset="utf-8">
+	<title>PHP Sample</title>
+</head>
+<body>
+<h1>Hello <?php echo $name; ?></h1>
+</body>
+</html>
