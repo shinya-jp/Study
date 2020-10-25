63601249890b22e2f2c1b37403195d1c0a94dd58
shinya-jp <shinya0733@gmail.com>
2020-10-25 22:20:44 +0900

    トレーニング

---
+++ PHP/web/web_tr1.php
@@ -0,0 +1,14 @@
+<?php
+$date = date("Y-m-d H:i:s");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>PHP Sample</title>
+</head>
+<body>
+<h1>Date:<?php echo $date; ?></h1>
+</body>
+</html>
