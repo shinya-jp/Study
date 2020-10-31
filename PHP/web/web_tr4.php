eef31cfbd4a15b70f5ec17d53f9af8a71928b0a1
shinya-jp <shinya0733@gmail.com>
2020-10-31 12:25:58 +0900

    テスト1

---
+++ PHP/web/web_tr4.php
@@ -0,0 +1,25 @@
+<?php
+ $x = $_POST["x"];
+ $y = $_POST["y"];
+ $total = $x + $y;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Sample</title>
+</head>
+<body>
+	<h3>Calc</h3>
+	<hr>
+	<p>
+		<?php echo $x; ?>
+		+
+		<?php echo $y; ?>
+		=
+		<?php echo $total; ?>
+	</p>
+</body>
+</html>
