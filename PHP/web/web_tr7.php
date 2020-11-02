32547bf074804b6cdb51c72ce6dd9c9d99f674e8
shinya-jp <shinya0733@gmail.com>
2020-11-02 19:17:20 +0900

    test1

---
+++ PHP/web/web_tr7.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$cart = [];
+if (isset($_SESSION["cart"])) {
+	$cart = $_SESSION["cart"];
+}
+if (isset($_GET["item"])) {
+	$cart = $_GET["item"];
+}
+$_SESSION["cart"] = $cart;
+
+$items = implode(",", $cart);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PHP Test</title>
+</head>
+<body>
+	<h3>Shop</h3>
+	<p>Cart: <?php echo $items; ?></p>
+	<hr>
+	<ul>
+		<li><a href="web_tr7.php?item=apple">Apple</a></li>
+		<li><a href="web_tr7.php?item=orange">Orange</a></li>
+		<li><a href="web_tr7.php?item=lemon">Lemon</a></li>
+	</ul>
+
+</body>
+</html>
